40d96b9db4a855fbf24d7a810ca30b5f64253dd2
Cathy Dao <cathydao95@gmail.com>
2022-06-14 16:43:15 -0700

    add put and delete

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const app = express();
 // body-parser used to extract data from form element
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
-const app = express();
 require("dotenv").config();
 
 let db,
@@ -15,9 +15,11 @@ MongoClient.connect(dbConnectionStr)
     const quotesCollection = db.collection("quotes");
 
     app.set("view engine", "ejs");
-
     // urlencoded tells body-parser to extract data from form element and add to body property in request object
+    app.use(express.static("public"));
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
     app.get("/", (request, response) => {
       //   response.sendFile(__dirname + "/index.html");
       //   const cursor = db.collection("quotes").find();
@@ -30,11 +32,44 @@ MongoClient.connect(dbConnectionStr)
         })
         .catch((error) => console.error(error));
     });
+
     app.post("/quotes", (request, response) => {
       quotesCollection
         .insertOne(request.body)
         .then((result) => {
           response.redirect("/");
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.put("/quotes", (request, response) => {
+      quotesCollection
+        .findOneAndUpdate(
+          { name: "Yoda" },
+          {
+            $set: {
+              name: request.body.name,
+              quote: request.body.quote,
+            },
+          },
+          {
+            upsert: true,
+          }
+        )
+        .then((result) => {
+          response.json("Success");
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.delete("/quotes", (request, response) => {
+      quotesCollection
+        .deleteOne({ name: request.body.name })
+        .then((result) => {
+          if (result.deletedCount === 0) {
+            return response.json("No quote to delete");
+          }
+          response.json(`Deleted Darth Vadar's quote`);
         })
         .catch((error) => console.error(error));
     });
